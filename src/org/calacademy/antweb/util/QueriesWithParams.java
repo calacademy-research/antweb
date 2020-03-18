b2bcff8987756d8beb1f9f72d16506b7793a8ec1
remarq <markjam1Git>
2020-03-17 21:11:31 -0700

    Release 8.15 Date: Mar 17   Modified build process.   Species list with Range Data Report.

--- src/org/calacademy/antweb/util/QueriesWithParams.java
+++ src/org/calacademy/antweb/util/QueriesWithParams.java
@@ -35,7 +35,7 @@ public abstract class QueriesWithParams {
                     "speciesListWithRangeData"
                     , "species list with range data for given geolocale."
                     , "<th>Subfamily</th><th>Genus</th><th>Species</th><th>Subspecies</th><th>Author Date</th><th>Status</th><th>Introduced</th><th>Endemic</th><th>Range</th>"
-                    , "select t.subfamily, t.genus, t.species, t.subspecies, t.author_date, t.status, gt.is_introduced, gt.is_endemic, "
+                    , "select t.subfamily, t.genus, t.species, IFNULL(t.subspecies, ''), t.author_date, t.status, gt.is_introduced, gt.is_endemic, "
                       + " (select group_concat(g2.name) from geolocale_taxon gt2, geolocale g2 where gt2.geolocale_id = g2.id and g2.georank = 'country' and gt2.taxon_name = t.taxon_name order by g2.name) "
                       + " from taxon t, geolocale_taxon gt, geolocale g where t.taxon_name = gt.taxon_name and gt.geolocale_id = g.id and t.rank in ('species', 'subspecies') and g.georank = 'country' and g.name = '" + stringParam + "' order by t.subfamily, t.genus, t.species, t.subspecies"
             );
