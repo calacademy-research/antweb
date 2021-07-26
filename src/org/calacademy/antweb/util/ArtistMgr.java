ff0acaaeefaf420695be693f505c23af313eb9b9
Dash Peters <dpeters@calacademy.org>
2021-07-26 10:26:03 -0700

    Clean up ArtistMgr imports, whitespace

--- src/org/calacademy/antweb/util/ArtistMgr.java
+++ src/org/calacademy/antweb/util/ArtistMgr.java
@@ -1,47 +1,40 @@
 package org.calacademy.antweb.util;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.*;
-
-import org.calacademy.antweb.*;
-import org.calacademy.antweb.geolocale.*;
-import org.calacademy.antweb.home.*;
-
-import org.apache.commons.logging.Log; 
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.calacademy.antweb.Artist;
+import org.calacademy.antweb.home.ArtistDb;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ArtistMgr extends Manager {
 
     private static final Log s_log = LogFactory.getLog(ArtistMgr.class);
 
     private static ArrayList<Artist> s_artists = null;
-    
+
     public static void populate(Connection connection, boolean forceReload, boolean initialRun) {
-      if (!forceReload && (s_artists != null)) return;      
-      
-      try {
-        //A.log("populate()");
-        ArtistDb artistDb = new ArtistDb(connection);
-        s_artists = artistDb.getArtists();
-      } catch (SQLException e) {
-        s_log.warn("populate() e:" + e);
-      }
+        if (!forceReload && (s_artists != null)) return;
 
-      if (!initialRun) {
-          try {
-              postInitialize(connection);
-          } catch (SQLException e) {
+        try {
+            //A.log("populate()");
+            ArtistDb artistDb = new ArtistDb(connection);
+            s_artists = artistDb.getArtists();
+        } catch (SQLException e) {
             s_log.warn("populate() e:" + e);
-          }
-      }
+        }
 
-      //A.log("GroupMgr.populate() groups:" + s_groups);
+        if (!initialRun) {
+            try {
+                postInitialize(connection);
+            } catch (SQLException e) {
+                s_log.warn("populate() e:" + e);
+            }
+        }
+
+        //A.log("GroupMgr.populate() groups:" + s_groups);
     }
 
     //Called through UtilAction to, in a separate thread, populate the curators with adm1.
@@ -52,32 +45,33 @@ public class ArtistMgr extends Manager {
     }
 
     public static boolean isInitialized() {
-      return s_artists != null;
+        return s_artists != null;
     }
 
     public static ArrayList<Artist> getArtists() {
-      return s_artists;
+        return s_artists;
     }
 
 
     public static Artist getArtist(int id) {
-      ArrayList<Artist> artists = getArtists();
-      if (artists == null) return null;
-      for (Artist artist : artists) {
-        if (id == artist.getId()) return artist;
-      }     
-      return null;
-    }
-    public static Artist getArtist(String name) {
-      ArrayList<Artist> artists = getArtists();
-      if (artists == null) return null;
-      for (Artist artist : artists) {       
-        if (name.equals(artist.getName())) {
-          //A.log("getArtist() name:" + name + " artist:" + artist);
-          return artist;
+        ArrayList<Artist> artists = getArtists();
+        if (artists == null) return null;
+        for (Artist artist : artists) {
+            if (id == artist.getId()) return artist;
         }
-      }     
-      return null;
+        return null;
+    }
+
+    public static Artist getArtist(String name) {
+        ArrayList<Artist> artists = getArtists();
+        if (artists == null) return null;
+        for (Artist artist : artists) {
+            if (name.equals(artist.getName())) {
+                //A.log("getArtist() name:" + name + " artist:" + artist);
+                return artist;
+            }
+        }
+        return null;
     }
 
 }
