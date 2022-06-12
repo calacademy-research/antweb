7107bb33107efb1a80d2bd8b866bbbd3d41b8659
Dash Peters <33206210+LordFlashmeow@users.noreply.github.com>
2022-06-12 11:43:30 -0700

    Fix taxonSetBackup.sh

--- bin/taxonSetBackup.sh
+++ bin/taxonSetBackup.sh
@@ -1,7 +1,7 @@
-# This script, launched weekly by cronjob, will dump data to /data/antweb/web/bak as a data protection.
+# This script, launched weekly by cronjob, will dump data to /mnt/antweb/web/bak as a data protection.
 
-today=`date +%Y-%m-%d`
-mysql -u antweb -pf0rm1c6 ant -e  "select g.name, gt.* from geolocale_taxon gt, geolocale g where gt.geolocale_id = g.id order by name, taxon_name" > /data/antweb/web/bak/taxonSets/geolocale/$today
-mysql -u antweb -pf0rm1c6 ant -e  "select * from proj_taxon order by project_name, taxon_name" > /data/antweb/web/bak/taxonSets/project/$today  
-mysql -u antweb -pf0rm1c6 ant -e  "select * from museum_taxon order by code" > /data/antweb/web/bak/taxonSets/museum/$today  
-mysql -u antweb -pf0rm1c6 ant -e  "select * from bioregion_taxon" > /data/antweb/web/bak/taxonSets/bioregion/$today  
+today=$(date +%Y-%m-%d)
+mysql -h mysql -u antweb -pf0rm1c6 ant -e "select g.name, gt.* from geolocale_taxon gt, geolocale g where gt.geolocale_id = g.id order by name, taxon_name" >/mnt/antweb/web/bak/taxonSets/geolocale/$today
+mysql -h mysql -u antweb -pf0rm1c6 ant -e "select * from proj_taxon order by project_name, taxon_name" >/mnt/antweb/web/bak/taxonSets/project/"$today"
+mysql -h mysql -u antweb -pf0rm1c6 ant -e "select * from museum_taxon order by code" >/mnt/antweb/web/bak/taxonSets/museum/"$today"
+mysql -h mysql -u antweb -pf0rm1c6 ant -e "select * from bioregion_taxon" >/mnt/antweb/web/bak/taxonSets/bioregion/"$today"
