ae6a1be1f7cd61115d276b3dc801c3ddb4497c07
Dash Peters <33206210+LordFlashmeow@users.noreply.github.com>
2022-06-28 09:45:10 -0700

    Update javamail jar, clean up Emailer.java, add email log

--- src/org/calacademy/antweb/util/Emailer.java
+++ src/org/calacademy/antweb/util/Emailer.java
@@ -4,57 +4,57 @@ import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 
-import org.apache.commons.logging.Log; 
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class Emailer
-{
+public class Emailer {
 
-   private static final Log s_log = LogFactory.getLog(Emailer.class);  
+    private static final Log s_log = LogFactory.getLog(Emailer.class);
+    private static final Log email_log = LogFactory.getLog("emailLog");
 
-   public static void main(String [] args) {
-   }
+    public static void main(String[] args) {
+    }
 
-   // Testable: https://www.antweb.org/util.do?action=emailTest
-   public static void sendMail(String to, String subject, String content) {
+    // Testable: https://www.antweb.org/util.do?action=emailTest
+    public static void sendMail(String to, String subject, String content) {
 
-		final String username = "antweb@calacademy.org";
-		final String passwd = "75txAKc8&%By";
+        final String username = "antweb@calacademy.org";
+        final String passwd = "75txAKc8&%By";
 
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
 
-		Session session = Session.getInstance(props,
-		  new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, passwd);
-			}
-		  }
-		);
+        Session session = Session.getInstance(props, new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, passwd);
+                    }
+                }
+        );
 
-		try {
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("antweb@calacademy.org"));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-			message.setSubject(subject);
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("antweb@calacademy.org"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
 
             //content = "<b>Content:</b>" + content;
             message.setContent("<html>\n" +
                     "<body>\n" +
                     content + "\n" +
                     "</body>\n" +
-                    "</html>", "text/html");			
-			
-			Transport.send(message);
+                    "</html>", "text/html");
 
-			s_log.warn("sendMail() done");
+            Transport.send(message);
 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
-    }  
+            email_log.info("Email sent to <" + to + "> with subject '" + subject + "'");
+
+        } catch (MessagingException e) {
+            s_log.error("Email to <" + to + "> with subject '" + subject + "' raised exception: " + e);
+//            throw new RuntimeException(e);
+        }
+    }
 
 }
