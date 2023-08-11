5bfe4089dd12004731481a49bdcdb2b9b214f216
Joe Russack <jrussack@calacademy.org>
2023-08-11 13:03:28 -0700

    path and line feed fix

--- bin/db-daily-dump.sh
+++ bin/db-daily-dump.sh
@@ -18,8 +18,10 @@ dbuser="antweb"
 dbpass="f0rm1c6"
 #backupdir="/data/antweb/web/db"
 #backupdir="/mnt/backup/db"
-rootbackupdir="/data/antweb/backup/db"
-backupdir="/data/antweb/backup/db/daily"
+# /mnt/antweb/backup/db/daily/
+# /mnt/backup/db/daily/
+rootbackupdir="/mnt/backup/db"
+backupdir="/mnt/backup/db/daily"
 
 curBak="$rootbackupdir"/"$dbname"-currentDump.sql.gz
 
@@ -29,8 +31,7 @@ datedBackupFile=$backupdir/"$date".sql.gz
 
 if [ -d $backupdir ]; then
   docker exec -it antweb_mysql_1 mysqldump -u $dbuser --password=$dbpass --routines  --skip-lock-tables --default-character-set=utf8 --all-databases --ignore-database 
-sys | gzip -c -9 > 
-"$tempfile"
+sys | gzip -c -9 > "$tempfile"
 #  mysqldump --skip-lock-tables -u$dbuser -p$dbpass -h mysql --all-databases --routines --single-transaction --quick --ignore-database sys | gzip -c -9 > "$tempfile"
   mv "$tempfile" "$datedBackupFile"
   echo "Copying $datedBackupFile to $curBak"
