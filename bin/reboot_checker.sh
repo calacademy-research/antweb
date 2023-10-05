4b6a05288191147475aebefe9b44b42e7f63e1f7
Mark Johnson <re.mark.johnson@gmail.com>
2023-10-05 04:43:46 -0700

    Refine

--- bin/reboot_checker.sh
+++ bin/reboot_checker.sh
@@ -7,15 +7,18 @@ page_content=$(curl -s "https://www.antweb.org/util.do?action=isRestart")
 now=$(date)
 
 # Check for 'true' or 'false' in the fetched content
-if [ $page_content == *"<b>false</b>"* ]; then
+if [[ $page_content == *"<b>false</b>"* ]]
+then
     echo "Diagnostic: 'false' found."
     exit 0
-elif [ $page_content == *"<b>true</b>"* ]; then
+elif [[ $page_content == *"<b>true</b>"* ]]
+then
     str="Diagnostic: 'true' found in rebootCheck.log. reboot: $now"
     echo $str
     echo $str >> logs/detail/rebootCheck.log
     exit 1
-elif [ $page_content == *"case#"* ]; then
+elif [[ $page_content == *"case#"* ]]
+then
     str="Diagnostic: 'case#' found in rebootCheck.log. reboot: $now"
     echo $str
     echo $str >> logs/detail/rebootCheck.log
