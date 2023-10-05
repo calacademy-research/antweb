6cc521bf9db43668b28f5972e6162c2ebae52394
Joe Russack <jrussack@calacademy.org>
2023-10-05 13:22:14 -0700

    fixed reboot checker

--- bin/reboot_checker.sh
+++ bin/reboot_checker.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
+log_path="../logs/detail/rebootCheck.log"
+
+cd "$(dirname "$0")"
+
 # Fetch the webpage
 page_content=$(curl -s "https://www.antweb.org/util.do?action=isRestart")
-#echo page_content: $page_content"" >> logs/detail/rebootCheck.log
-
 now=$(date)
 
-# Check for 'true' or 'false' in the fetched content
-if [[ $page_content == *"<b>false</b>"* ]]
-then
-    echo "Diagnostic: 'false' found."
-    exit 0
-elif [[ $page_content == *"<b>true</b>"* ]]
-then
+# Define diagnostic string based on page content
+if [[ "$page_content" == *"<b>false</b>"* ]]; then
+    str="Diagnostic: 'false' found."
+    exit_code=0
+elif [[ "$page_content" == *"<b>true</b>"* ]]; then
     str="Diagnostic: 'true' found in rebootCheck.log. reboot: $now"
-    echo $str
-    echo $str >> logs/detail/rebootCheck.log
-    exit 1
-elif [[ $page_content == *"case#"* ]]
-then
+    exit_code=1
+elif [[ "$page_content" == *"case#"* ]]; then
     str="Diagnostic: 'case#' found in rebootCheck.log. reboot: $now"
-    echo $str
-    echo $str >> logs/detail/rebootCheck.log
-    exit 1
+    exit_code=1
 else
-    echo "Warning: Neither 'true' nor 'false' found."
-    exit 1
+    str="Warning: Neither 'true' nor 'false' found in page. $now"
+    exit_code=1
 fi
 
+# Output diagnostic string
+echo $str
+echo $str >> $log_path
+exit $exit_code
 
