7da32459e39e93a670b6ac5e745888d6887521fb
Mark Johnson <re.mark.johnson@gmail.com>
2023-10-05 04:38:21 -0700

    refine

--- bin/reboot_checker.sh
+++ bin/reboot_checker.sh
@@ -10,12 +10,12 @@ now=$(date)
 if [ $page_content == *"<b>false</b>"* ]; then
     echo "Diagnostic: 'false' found."
     exit 0
-elif [[ $page_content == *"<b>true</b>"* ]]; then
+elif [ $page_content == *"<b>true</b>"* ]; then
     str="Diagnostic: 'true' found in rebootCheck.log. reboot: $now"
     echo $str
     echo $str >> logs/detail/rebootCheck.log
     exit 1
-elif [[ $page_content == *"case#"* ]]; then
+elif [ $page_content == *"case#"* ]; then
     str="Diagnostic: 'case#' found in rebootCheck.log. reboot: $now"
     echo $str
     echo $str >> logs/detail/rebootCheck.log
