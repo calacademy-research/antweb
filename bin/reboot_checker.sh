63ecba4ab0af9d30ed5d81d765bb461961724668
Mark Johnson <re.mark.johnson@gmail.com>
2023-10-05 14:13:35 -0700

    log line change.

--- bin/reboot_checker.sh
+++ bin/reboot_checker.sh
@@ -13,10 +13,10 @@ if [[ "$page_content" == *"<b>false</b>"* ]]; then
     str="Diagnostic: 'false' found."
     exit_code=0
 elif [[ "$page_content" == *"<b>true</b>"* ]]; then
-    str="Diagnostic: 'true' found in rebootCheck.log. reboot: $now"
+    str="Diagnostic: 'true' found in reboot_checker.sh. Reboot: $now"
     exit_code=1
 elif [[ "$page_content" == *"case#"* ]]; then
-    str="Diagnostic: 'case#' found in rebootCheck.log. reboot: $now"
+    str="Diagnostic: 'case#' found in reboot_checker.sh. Reboot: $now"
     exit_code=1
 else
     str="Warning: Neither 'true' nor 'false' found in page. $now"
