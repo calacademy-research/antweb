0aae442f99effa1abc5eacd65be20da0eaa9507e
Mark Johnson <re.mark.johnson@gmail.com>
2023-10-05 04:37:46 -0700

    refine

--- bin/reboot_checker.sh
+++ bin/reboot_checker.sh
@@ -7,7 +7,7 @@ page_content=$(curl -s "https://www.antweb.org/util.do?action=isRestart")
 now=$(date)
 
 # Check for 'true' or 'false' in the fetched content
-if [[ $page_content == *"<b>false</b>"* ]]; then
+if [ $page_content == *"<b>false</b>"* ]; then
     echo "Diagnostic: 'false' found."
     exit 0
 elif [[ $page_content == *"<b>true</b>"* ]]; then
