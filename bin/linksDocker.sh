fd165be06e2a1e6267f18ee0b4a467d92b7ec5ea
Mark Johnson <re.mark.johnson@gmail.com>
2024-06-19 01:12:54 -0700

    refine

--- bin/linksDocker.sh
+++ bin/linksDocker.sh
@@ -2,19 +2,32 @@
 # This file is useful, on production and stage, for creating a set of handy softlinks to access
 # resoures.  Recommended, in a directory called links, execute this script, like:
 #
-# sh /home/antweb/antweb/bin/links.sh
+# After
+# Invoke from the links directory
+# sh ../deploy/bin/linksDocker.sh
 
 
 rm *
 
-#ln -s /usr/local/apache/logs/access_log access_log
 ln -s /usr/local/tomcat tomcat
 ln -s tomcat/logs/antweb.log antweb.log
 ln -s tomcat/logs/antwebInfo.log antwebInfo.log
+ln -s tomcat/work/Catalina/localhost/ROOT/org/apache/jsp/ jspComp
+
+ln -s /usr/local/antweb/web/log/ webLogs
+ln -s /usr/local/tomcat/logs tomcatLogs
+
+ln -s /data/antweb/log/ antLogs
+
+ln -s /mnt/antweb/web/ webMnt
+
+
+
+
+#ln -s /usr/local/apache/logs/access_log access_log
 #ln -s tomcat/logs/appCheck.log appCheck.log
 #ln -s /var/www/html/imageUpload/ imageUpload
 #ln -s /home/antweb/workingdir workingdir
-
 
 
 #lrwxrwxrwx 1 antweb antweb   33 Jun 17 18:16 access_log -> /usr/local/apache/logs/access_log
