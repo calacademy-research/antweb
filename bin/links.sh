d61122566b19d364cb2afca262fa8ed2a38cdd42
Mark Johnson <re.mark.johnson@gmail.com>
2024-06-19 01:24:43 -0700

    refine

--- bin/links.sh
+++ bin/links.sh
@@ -4,11 +4,20 @@
 #
 # sh /home/antweb/antweb/bin/links.sh
 
-ln -s /usr/local/tomcat tomcat
-ln -s tomcat/logs/antweb.log antweb.log
-ln -s tomcat/logs/antwebInfo.log antwebInfo.log
-ln -s tomcat/logs/appCheck.log appCheck.log
-ln -s /usr/local/apache/logs/access_log access_log
-ln -s /usr/local/apache/logs/error_log error_log
-ln -s /var/www/html/imageUpload/ imageUpload
-ln -s /home/antweb/workingdir workingdir
+rm *
+
+ln -s /mnt mnt
+ln -s mnt/backup/db dbBackup
+
+ln -s mnt/antweb/web/ webMnt
+ln -s mnt/antweb/web/bak/taxonSets taxonSetsBak
+
+
+#ln -s /usr/local/tomcat tomcat
+#ln -s tomcat/logs/antweb.log antweb.log
+#ln -s tomcat/logs/antwebInfo.log antwebInfo.log
+#ln -s tomcat/logs/appCheck.log appCheck.log
+#ln -s /usr/local/apache/logs/access_log access_log
+#ln -s /usr/local/apache/logs/error_log error_log
+#ln -s /var/www/html/imageUpload/ imageUpload
+#ln -s /home/antweb/workingdir workingdir
